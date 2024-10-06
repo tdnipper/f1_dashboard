efab585ad73f68524419ec9809d4fa6e3c8605f3
Thomas <thomasnipper@proton.me>
2024-10-05 21:18:24 -0500

    Add data visualization for lap durations

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
 from urllib.request import urlopen
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
 
-response = urlopen('https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159')
+data_type = "laps"
+drive_number = '55'
+session_key = "9159"
+
+response = urlopen(
+    f"https://api.openf1.org/v1/{data_type}?driver_number={drive_number}&session_key={session_key}"
+)
 data_json = json.loads(response.read().decode("utf-8"))
 data = pd.json_normalize(data_json)
 data.to_excel("test.xlsx")
+
+plt.plot(data["lap_number"], data["lap_duration"])
+plt.tight_layout()
+plt.show()
