168fa76b8d96f74ebeebfe4268c91062ce69680c
Thomas <thomasnipper@proton.me>
2024-10-06 17:36:14 -0500

    clean up commented code and add fetch_stint_data function and plot

--- get_data.py
+++ get_data.py
@@ -1,12 +1,9 @@
 import pandas as pd
-import streamlit as st
 import requests
 import plotly.express as px
-import json
 
 data_type = "laps"
 drive_number = '55'
-session_key = "9159"
 
 # Get the session key for a given country, session type and year
 def fetch_session_key(country, session_type, year):
@@ -24,6 +21,10 @@ year = '2023'
 
 session_key = fetch_session_key(country, session_type, year)
 
+data_type = "laps"
+drive_number = '55'
+
+# Fetch data for a given data type, driver number and session key
 def fetch_data(data_type, drive_number, session_key):
     url = f"https://api.openf1.org/v1/{data_type}?driver_number={drive_number}&session_key={session_key}"
     response = requests.get(url)
@@ -32,8 +33,20 @@ def fetch_data(data_type, drive_number, session_key):
     else:
         raise ValueError(f"Error fetching data. Status code {response.status_code}")
     
-# data = fetch_data(data_type, drive_number, session_key)
-# # data.to_csv('data.csv', index=False)
-# fig = px.line(data, x='lap_number', y='lap_duration', title=f"Lap times for driver {drive_number}")
-# fig.show()
+def fetch_stint_data(session_key, driver_number):
+    url = f"https://api.openf1.org/v1/stints?session_key={session_key}&driver_number={driver_number}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return pd.json_normalize(response.json())
+    else:
+        raise ValueError(f"Error fetching data. Status code {response.status_code}")
+    
+race_data = fetch_data(data_type, drive_number, session_key)
+stint_data = fetch_stint_data(session_key, drive_number)
+
+# data.to_csv('data.csv', index=False)
+fig_race = px.scatter(race_data, x='lap_number', y='lap_duration', title=f"Lap times for driver {drive_number}", color='is_pit_out_lap', trendline='ols')
+fig_race.show()
+
+fig_stint = px.bar(stint_data, x='stint_number', y='stint_duration', title=f"Stint times for driver {drive_number}")
 
